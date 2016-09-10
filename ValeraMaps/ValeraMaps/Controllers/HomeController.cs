01fe30c97b479bf98e5d213a272e4d4ceadd563d
walera <becwal@yandex.ru>
2016-09-10 20:52:26 +0300

    Create git of ValeraMaps

---
+++ ValeraMaps/ValeraMaps/Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ValeraMaps.Models;
+
+namespace ValeraMaps.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        public JsonResult GetData()
+        {
+            // создадим список данных
+            List<People> pp = new List<People>();
+
+            pp.Add(
+                new People {
+                    name="Валера",
+                    numform="791",
+                    GeoLat = 37.608644,
+                    GeoLong = 55.75226,
+                    info ="Валера вместо прохождения Морфлота гуляет по Александровскому саду",
+                    photolink= "http://www.videosharp.info/users/791/avatar/791_avatar_1.png"
+
+                }
+                
+                );
+
+            pp.Add(
+               new People
+               {
+                   name = "Оля Муза",
+                   numform = "24",
+                   GeoLat = 37.610489,
+                   GeoLong = 55.752308,
+                   info = "Оля пополняет багаж знаний в библиотеке им Ленина",
+                   photolink = "http://www.videosharp.info/users/24/avatar/24_avatar_1.jpg"
+
+               }
+
+               );
+
+
+            return Json(pp, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ ValeraMaps/ValeraMaps/Models/People.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ValeraMaps.Models
+{
+    public class People
+    {
+        public string id { get; set; }
+        public string name { get; set; } //имя формулиста
+        public string numform { get; set; } //номер формулиста
+        public double GeoLong { get; set; } // долгота - для карт google
+        public double GeoLat { get; set; } // широта - для карт google
+        public string info { get; set; } //информация
+        public string photolink { get; set; } //ссылка на фото
+
+    }
+}
